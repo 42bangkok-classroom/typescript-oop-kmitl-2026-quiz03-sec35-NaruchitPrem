c12932911a8ad7b3b8b5be1d1a983e91712b83a3
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-02-16 10:10:45 +0700

    quiz3

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,18 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+    generation: number;
+
+    constructor(name: string, generation: number, price: number) {
+        super(name, price);
+        this.generation = generation;
+    }
+
+    getProfile(): string {
+        return `${this.name} (Gen ${this.generation})`;
+    }
+
+    getDiscountPrice(): number {
+        return this.price - (this.price * Product.DISCOUNT_PERCENT / 100);
+    }
 }
