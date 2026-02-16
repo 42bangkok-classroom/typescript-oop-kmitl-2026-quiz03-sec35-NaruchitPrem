60d3f6977506d1ef8b334b7cb9b4726f9d3aef45
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-02-16 09:23:04 +0700

    quiz2

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,20 @@
-export class Person {}
+export class Person {
+    firstname: string = '';
+    lastname: string = '';
+
+    private _age: number = 0;
+
+    static COUNTRY: string = 'THAILAND';
+
+    setAge(age: number): void {
+        this._age = age;
+    }
+
+    getAge(): number {
+        return this._age;
+    }
+
+    getFullName(): string  {
+        return `${this.firstname} ${this.lastname}`
+    }
+}
